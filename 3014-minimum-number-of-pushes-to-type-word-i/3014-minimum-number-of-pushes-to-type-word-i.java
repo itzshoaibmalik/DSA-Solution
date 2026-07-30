4a7bff9c9db9045d2f8b2c7dd3611218002451b6
Shoaib Malik <70048082+itzshoaibmalik@users.noreply.github.com>
2026-07-30 16:54:51 +0530

    Time: 0 ms (100%), Space: 43.3 MB (28.23%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+
+        int ans = 0; 
+        int k = 1;
+
+        for(int i = 0; i < n / 8; ++i){
+            ans += k * 8;
+            k++;
+        }
+        ans += k * (n % 8);
+        return ans;
+    }
+}
